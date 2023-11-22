acd4451a6be175018a7676773db39155a4c2bc35
pvsabhishek <36543695+pvsabhishek@users.noreply.github.com>
2023-11-22 07:51:46 +0000

    comparison of datatype

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1); // equal to 
+// console.log(2 != 1); // not equal to
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+
+// AVOID THIS TYPE OF COMPARISON
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+// === strict check
+
+console.log("2" === 2);
