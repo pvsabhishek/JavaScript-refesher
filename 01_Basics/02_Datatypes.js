17d869be864d55c64e0a75d4a71468a4dbef941f
pvsabhishek <36543695+pvsabhishek@users.noreply.github.com>
2023-11-22 07:08:09 +0000

    Datatypes and ECMA standards

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3 
+    + 
+    
+    2);  // code readability should be high
+
+console.log("Abhishek");
+
+let name = "abhishek"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+//TYPES OF DATATYPE
+
+// PRIMITIVE DATATYPES
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean = true/false
+// null => standalone value
+// undefined => not given a value to variables
+// symbol => unique
+
+
+// object
+
+console.log(typeof "chiku"); // declare type of value
+console.log(typeof null); // object
