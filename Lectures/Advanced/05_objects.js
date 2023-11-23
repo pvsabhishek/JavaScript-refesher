641c3772bc1b8e3c3286d2de0631d7b2316510df
pvsabhishek <36543695+pvsabhishek@users.noreply.github.com>
2023-11-23 16:04:45 +0000

    completed objects

---
+++ Lectures/Advanced/05_objects.js
@@ -0,0 +1,31 @@
+const course = {
+    coursename: 'js in hindi',
+    price: '999',
+    courseInstructor: 'chiku'
+}
+
+// course.courseInstructor
+
+const {courseInstructor : instructor} = course
+// console.log(courseInstructor);
+console.log(instructor);
+
+// const navbar = ({company}) => {
+// }
+// navbar(company = "çhiku")
+
+
+// JSON
+// {
+//     'name':'chiku',
+//     'courseName': 'js in english',
+//     'price':'free',
+// }
+
+
+// array of onjects
+// [
+//     {},
+//     {},
+//     {},
+// ]
